749bb04e1fe75ff71419eab2dc0c229b57ccc35b
Suica-com-cafe <102829750+Suica-com-cafe@users.noreply.github.com>
2022-09-02 16:17:44 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+var rope;
+
+
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(200,680,600,20);
+  rope - new Rope(6,{x:245,y:30});
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  rope.show();
+  
+  Engine.update(engine);
+  
+  
+ 
+   
+}
